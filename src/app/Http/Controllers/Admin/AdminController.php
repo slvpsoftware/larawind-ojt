11f5efe23122b539394caeeaaed2aa0aa870d3f8
ChrisIanApuhin <chrisian.bpoc.ojt@gmail.com>
2023-04-05 10:48:46 +0800

    testing push

--- src/app/Http/Controllers/Admin/AdminController.php
+++ src/app/Http/Controllers/Admin/AdminController.php
@@ -11,7 +11,7 @@ class AdminController extends Controller
 {
     public function adminlogin(Request $request)
     {
-        
+        dd($request->all());
         
     }
     public function login()

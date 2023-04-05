fbaaed052c1ebbdafd91ead430a5c4c9205d33af
ChrisIanApuhin <chrisian.bpoc.ojt@gmail.com>
2023-04-05 11:39:43 +0800

    login authentication

--- src/app/Http/Controllers/Admin/AdminController.php
+++ src/app/Http/Controllers/Admin/AdminController.php
@@ -5,33 +5,48 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Admin;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function adminlogin(Request $request)
     {
-        dd($request->all());
+        // dd($request->all());
+        $credentials = [
+            'username' => $request->username,
+            'password' => $request->password
+        ];
+        if (Auth::guard('admin')->attempt($credentials))//check the credentials
+         {
+            dd(Auth::guard('admin')->user()->id);//display the user id if successfully logged in
+        }
+        else
+        {
+            dd('Invalid Credentials');
+        }
+
+    //    dd($credentials);
         
     }
-    public function login()
+    public function login()//display the log in page
     {
         return view('pages.welcome');
     }
 
-    public function register()
+    public function register()//display the register page
     {
         return view('pages.signup');
     }
 
-    public function signup(Request $request)
+    public function signup(Request $request)//form that accepts registration details
     {
        $admin = new Admin();
        $admin->username = $request->username;
        $admin->password = Hash::make($request->password);
     //    $isGoods =
-        $admin->save();
-    //    dd($isGoods);
-        return 'Welcome';
+        $admin->save();//save to database
+    
+        return 'Account Created Successfully';
     }
 }

e106492b8a1cd7fab65fd5457a00b8d8c92faef0
ChrisIanApuhin <chrisian.bpoc.ojt@gmail.com>
2023-04-18 17:35:06 +0800

    final search function

--- src/resources/views/pages/viewproduct.blade.php
+++ src/resources/views/pages/viewproduct.blade.php
@@ -170,57 +170,11 @@
         </tr>
       </tbody>
       @endforeach
-      <div class="row">
-        <div class="col md-12">
-           {{$products->links('pagination::tailwind')}}
-        </div>
-    </div>
+      
     </table>
-    
-    {{-- Sweetalert Delete --}}
-    {{-- <script>
-      window.addEventListener('delete-confirmation', event => {
-        Swal.fire(
-        {
-          title: 'Are you sure?',
-          text: "You won't be able to revert this!",
-          icon: 'warning',
-          showCancelButton: true,
-          confirmButtonColor: '#3085d6',
-          cancelButtonColor: '#d33',
-          confirmButtonText: 'Yes, delete it!'
-        }).then((result) => 
-        {
-          if (result.isConfirmed) 
-          {
-           Livewire.emit('deleteProduct');
-          }
-        })
-      });
-
-
-      // Swal.fire(
-      //   {
-      //     title: 'Are you sure?',
-      //     text: "You won't be able to revert this!",
-      //     icon: 'warning',
-      //     showCancelButton: true,
-      //     confirmButtonColor: '#3085d6',
-      //     cancelButtonColor: '#d33',
-      //     confirmButtonText: 'Yes, delete it!'
-      //   }).then((result) => 
-      //   {
-      //     if (result.isConfirmed) 
-      //     {
-      //       Swal.fire(
-      //         'Deleted!',
-      //         'Your file has been deleted.',
-      //         'success'
-      //       )
-      //     }
-      //   }
-      //   )
-    </script> --}}
+         {{$products->links('pagination::tailwind')}}
+      
+ 
    
   </div>
   

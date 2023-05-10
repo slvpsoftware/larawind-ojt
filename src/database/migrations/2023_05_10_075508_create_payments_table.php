97b2c9d264c5fb3cf8bea73ab44799aa5f2663fa
geneveve-bpoc <geneveve@bpoc.co.jp>
2023-05-10 16:03:02 +0800

    migration and model for payment

---
+++ src/database/migrations/2023_05_10_075508_create_payments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->string("customer_id");
+            $table->string("checkout_id");
+            $table->string("product_id");
+            $table->string("name_of_card");
+            $table->string("card_number");
+            $table->string("expiry_date");
+            $table->string("cvv");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
